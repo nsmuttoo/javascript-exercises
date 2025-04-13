753308fa744d598d9ca1670c306e2fd41c6fd1ca
nsmuttoo <nsmuttoo@gmail.com>
2025-04-13 17:31:43 -0400

    Completed exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(input) {
+output = ''
 
+for(i = 0; i<=input.length; i++){
+    
+    output = output + input.charAt(input.length-i)
+}
+
+return output;
 };
 
 // Do not edit below this line
