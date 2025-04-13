62da59b7034def8420311732647dc67e918ae00e
nsmuttoo <nsmuttoo@gmail.com>
2025-04-13 17:21:10 -0400

    Completed exercises 1 & 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(phrase, count) {
+combinedPhrase = ""
+if(count<0){
+    return 'ERROR'
+}
+for( i =0; i<count; i++ ){
+    combinedPhrase = combinedPhrase+phrase
+}
+console.log(combinedPhrase)
+return combinedPhrase
 };
 
 // Do not edit below this line
