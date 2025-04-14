a2d4ee5ca73b0e21d66976f75ac2fe33d7e8bdee
nsmuttoo <nsmuttoo@gmail.com>
2025-04-14 15:40:53 -0400

    Completed Exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(numb1, numb2) {
+low = 0
+high = 0
+count = 0
+if(Number.isInteger(numb1) && Number.isInteger(numb2) && numb1>=0 && numb2 >=0){
 
+}else{
+    return 'ERROR'
+}
+
+    if(numb1 > numb2){
+    low = numb2
+    high = numb1
+}else{
+    low = numb1
+    high = numb2
+}
+for(i = low; i<=high; i++ ){
+    count = count +i
+}
+return count
 };
 
 // Do not edit below this line
