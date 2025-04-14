e468ffb058ba17288ca00076c03cbc4d07b00834
nsmuttoo <nsmuttoo@gmail.com>
2025-04-14 15:21:58 -0400

    Exercise 4 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(input, remover1, remover2, remover3, remover4)  {
+output = []
+count = 0;
+
+    for(i = 0; i < input.length; i++){
+       
+        if(input[i] !== remover1 && input[i] !== remover2 && input[i] !== remover3 && input[i] !== remover4){
+            output[count] = input[i]
+            count++
+        }
+
+
+}
+return output
 };
 
 // Do not edit below this line
