52e0a4e7b8a4432a2ef78add8f858e72ac0064e7
nsmuttoo <nsmuttoo@gmail.com>
2025-04-14 15:53:27 -0400

    Completed exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  output = (temp-32)*5/9
+  return Math.round(output*10)/10
 };
+const convertToFahrenheit = function(temp) {
+  output = temp*9/5 +32
 
-const convertToFahrenheit = function() {
-};
+  return Math.round(output*10)/10
+  };
+
 
 // Do not edit below this line
 module.exports = {
