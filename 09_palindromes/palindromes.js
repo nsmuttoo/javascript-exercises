e1ffed7a9cfcb08fd24ccf30aa6951d938e6c3c4
nsmuttoo <nsmuttoo@gmail.com>
2025-04-21 15:20:33 -0400

    Completed exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+const palindromes = function (word) {
+let reversed = word.split("").reverse().join("")
 
+
+
+word = word.toLowerCase()
+reversed = reversed.toLowerCase()
+word = word.replace("!","")
+word =word.replace(/\s+/g, '')
+word =word.replace(".","")
+word =word.replace(/,/g, '')
+reversed =reversed.replace("!","")
+reversed =reversed.replace(/\s+/g, '')
+reversed =reversed.replace(".","")
+reversed =reversed.replace(/,/g, '')
+console.log(reversed)
+console.log(word)
+if(word == reversed){
+return true
+}
+return false
 };
 
 // Do not edit below this line
