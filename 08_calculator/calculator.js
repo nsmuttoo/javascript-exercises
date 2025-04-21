2aa7fcfdbc12572351311916bc0ab5915eaf7437
nsmuttoo <nsmuttoo@gmail.com>
2025-04-21 14:17:24 -0400

    Completed exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	return num1+num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1-num2
 };
 
-const sum = function() {
-	
+const sum = function(input) {
+if(input === undefined || input.length === 0){
+  return 0 
+}
+
+	const output = input.reduce((total,currentItem)=>{
+    return total+currentItem
+  }
+  )
+return output
 };
 
-const multiply = function() {
-
+const multiply = function(input) {
+  if(input === undefined || input.length === 0){
+    return 0 
+  }
+  
+    const output = input.reduce((total,currentItem)=>{
+      return total*currentItem
+    }
+    )
+  return output
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	let total = 1
+  for( i=0; i<power; i++){
+    total = total*num
+  }
+  return total
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1
+  for( i=0; i<num; i++){
+    total = total*(num-i)
+  }
+  return total
 };
 
 // Do not edit below this line
