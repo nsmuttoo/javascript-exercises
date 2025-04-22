0e3ea29fa9322f1d0de66a8db0e3220c1b34afbb
nsmuttoo <nsmuttoo@gmail.com>
2025-04-22 14:40:47 -0400

    Completed exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(input) {
+store = 0
+console.log(input[store].name)
+for(i=0; i<input.length; i++){
+if (getAge(input[i])>getAge(input[store])){
+    store = i
+    console.log(input[store].name)
+}
+}
+named = input[store].name
+console.log("name is" +named)
+return input[store]
 };
+
+function getAge(person){
+if(person.yearOfDeath == undefined){
+    return (2025-person.yearOfBirth)
+}
+return (person.yearOfDeath-person.yearOfBirth)
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
