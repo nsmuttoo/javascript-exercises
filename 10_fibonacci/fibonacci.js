fe7cbe9f6444f6b4f81054a88287902bc4b884c3
nsmuttoo <nsmuttoo@gmail.com>
2025-04-22 14:18:49 -0400

    Completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+if(num<0){
+    return "OOPS"
+}
 
+    sequence = [0,1,1]
+for (i =3; i<=num; i++){
+    sequence[i] = sequence[i-1] + sequence[i-2]
+    console.log(sequence[i])
+}
+return sequence[num]
 };
 
 // Do not edit below this line
