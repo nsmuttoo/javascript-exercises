197faaf08a87a279c8d5518146b9a7ff12c1a76a
nsmuttoo <nsmuttoo@gmail.com>
2025-04-22 14:22:46 -0400

    Completed exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,14 @@
-const getTheTitles = function() {
+const getTheTitles = function(input) {
 
+
+
+const output = input.map(getTitle)
+return output
 };
+
+function getTitle(book){
+return book.title
+}
 
 // Do not edit below this line
 module.exports = getTheTitles;
